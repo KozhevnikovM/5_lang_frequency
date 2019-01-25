e56ea4a18620f3bf476a6f84a2b906199e936944
Info Supsys <info@supsys.cc>
2019-01-25 18:18:52 +0300

    add collection.Counter

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+The script return 10 most frequency words in a text
+
+# System requirements
+
+Python3.5
+
+# Quickstart
+python lang_frequency.py path_to_text
+```bash
+$ python lang_frequency.py text.txt
+the
+of
+a
+Lorem
+and
+Ipsum
+in
+is
+it
+has
+```
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,32 @@
+import os, sys
+from collections import Counter
 
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+        return 'File Not Found'
+    with open(filepath, 'r', encoding='utf8') as f:
+        return f.read().strip()
 
 
 def get_most_frequent_words(text):
-    pass
+    words = calc_words(text)
+    return sorted(words.keys(), reverse=True, key=words.get)
+
+
+def calc_words(text):
+    counter = Counter()
+    words = text.strip().split()
+    for word in words:
+        counter[word] += 1
+    return counter
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) == 2:
+        text = sys.argv[1]
+    else:
+        print('Укажите путь к файлу')
+    text = load_data('text.txt')
+    for i in get_most_frequent_words(text)[:10]:
+        print(i)
